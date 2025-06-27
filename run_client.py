b789f03d20ff0ea3e5c597300fdc093dcf03f6d2
ai-core-tools <ai.core.tools@gmail.com>
2025-06-27 23:04:28 +0300

    py client script retries fixed

--- run_client.py
+++ run_client.py
@@ -9,14 +9,18 @@ from hsu_core.py.control.client_conn import ClientConn
 from hsu_core.py.control.gateway import Gateway as CoreGateway
 from py.control.gateway import Gateway as EchoGateway
 
-client_conn = ClientConn(args.port)
-core_gateway = CoreGateway(client_conn.GRPC())
-echo_gateway = EchoGateway(client_conn.GRPC())
-
 retry_period = 1
 while True:
     try:
-        response = core_gateway.Ping()
+        client_conn = ClientConn(args.port)
+        core_gateway = CoreGateway(client_conn.GRPC())
+        echo_gateway = EchoGateway(client_conn.GRPC())
+
+        core_gateway.Ping()
+
+        message = echo_gateway.Echo("Hello, World!")
+        print(message)
+
         break
     except Exception as e:
         if "ConnectEx: Connection refused (No connection could be made because the target machine actively refused it." in str(e):
@@ -27,6 +31,3 @@ while True:
         else:
             print(e)
             raise e
-
-message = echo_gateway.Echo("Hello, World!")
-print(message)
